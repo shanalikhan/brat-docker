6704fc61785ae48b02b4afc8c8e6b6a9cfbb17ad
Tristan Crockett <tristan.h.crockett@gmail.com>
2018-04-08 10:34:18 -0500

    Instead of new config template, modify install wrapper to append the users

--- brat_install_wrapper.sh
+++ brat_install_wrapper.sh
@@ -5,11 +5,9 @@ $BRAT_USERNAME
 $BRAT_PASSWORD 
 $BRAT_EMAIL
 EOD
+echo "import json; USER_PASSWORD.update(json.load(open('cfg/users.json', 'r'))" >> /var/www/brat/brat-v1.3_Crunchy_Frog/config.py
 
 chown -R www-data:www-data /bratdata
-
-# patch the user config with more users
-python /var/www/brat/brat-v1.3_Crunchy_Frog/user_patch.py
 
 echo "Install complete. You can log in as: $BRAT_USERNAME"
 
